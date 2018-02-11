b482416b7f273ba0d3d22a1eabfe53c86fd8b6bb
José Sousa <josesousa9000@gmail.com>
2018-02-11 23:18:55 +0000

    :sunrise:

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a href="https://www.twitter.com/josesousa9000" className="icon fa-twitter">
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.github.com/jfranciscosousa" className="icon fa-github">
+                <span className="label">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="josesousa9000@gmail.com" className="icon fa-envelope-o">
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="copyright">
+            <li>
+              Design based on Strata by{' '}
+              <a href="http://html5up.net">HTML5 UP</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer
