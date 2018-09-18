8a6fa2231208b855974ecc777abdae97bb6dcef0
José Francisco Sousa <josesousa9000@gmail.com>
2018-09-18 14:57:06 +0100

    Update most packages, and Gatsby 2

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,36 +1,39 @@
 import React from "react";
 
-import icons from "simple-line-icons/css/simple-line-icons.css"; // eslint-disable-line no-unused-vars
-import styles from "./index.css"; // eslint-disable-line no-unused-vars
+import "./index.module.css";
 
 export default class SocialLinks extends React.Component {
   render() {
     return (
-      <div styleName="styles.root">
+      <div styleName="root">
         <a
           href="https://www.twitter.com/xicomeme"
-          styleName="styles.icon icons.icon-social-twitter"
+          styleName="icon"
+          className="icon-social-twitter"
         >
           <span style={{ display: "none" }}>Twitter</span>
         </a>
 
         <a
           href="https://www.github.com/jfranciscosousa"
-          styleName="styles.icon icons.icon-social-github"
+          styleName="icon"
+          className="icon-social-github"
         >
           <span style={{ display: "none" }}>Github</span>
         </a>
 
         <a
           href="https://www.linkedin.com/in/jfranciscosousa/"
-          styleName="styles.icon icons.icon-social-linkedin"
+          styleName="icon"
+          className="icon-social-linkedin"
         >
           <span style={{ display: "none" }}>LinkedIn</span>
         </a>
 
         <a
           href="mailto:josesousa9000@gmail.com"
-          styleName="styles.icon icons.icon-envelope-letter"
+          styleName="icon"
+          className="icon-envelope-letter"
         >
           <span style={{ display: "none" }}>Mail</span>
         </a>
