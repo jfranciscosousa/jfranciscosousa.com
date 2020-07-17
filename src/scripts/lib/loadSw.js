791370b29f3fcf39e5b8580c638a8e3afcea761f
Francisco Sousa <francisco.sousa@hey.com>
2020-07-17 18:25:56 +0100

    Fix

--- src/scripts/lib/loadSw.js
+++ src/scripts/lib/loadSw.js
@@ -4,19 +4,7 @@ module.exports = {
   init: () => {
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").then(
-          (registration) => {
-            // Registration was successful
-            console.log(
-              "ServiceWorker registration successful with scope: ",
-              registration.scope,
-            );
-          },
-          (err) => {
-            // registration failed :(
-            console.log("ServiceWorker registration failed: ", err);
-          },
-        );
+        navigator.serviceWorker.register("/service-worker.js");
       });
     }
   },
