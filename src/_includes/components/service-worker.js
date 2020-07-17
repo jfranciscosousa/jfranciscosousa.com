0310ec0dee7ba6fbf17879d57059059ae7462614
Francisco Sousa <francisco.sousa@hey.com>
2020-07-17 18:30:31 +0100

    Fix

--- src/_includes/components/service-worker.js
+++ src/_includes/components/service-worker.js
@@ -21,7 +21,7 @@ const PRE_CACHE_URLS = [
 ];
 
 // You might want to bypass a certain host
-const IGNORED_HOSTS = [];
+const IGNORED_HOSTS = ["localhost"];
 
 /**
  * Takes an array of strings and puts them in a named cache store
