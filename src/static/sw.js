5098156b8e39eb9dda47f6825d88c2eeee62fa83
Francisco Sousa <francisco.sousa@hey.com>
2020-07-17 14:08:27 +0100

    SW

---
+++ src/static/sw.js
@@ -0,0 +1,52 @@
+const cacheName = "jfranciscosousa-sw1";
+const filesToCache = [
+  "turbolinks.js",
+  "favicon.ico",
+  "/",
+  "/about",
+  "/projects",
+  "/books",
+  "/blog",
+];
+
+const { self, caches, fetch } = this;
+
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  console.log("[Servicework] Install");
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[ServiceWorker] Caching app shell");
+
+      return cache.addAll(filesToCache);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[Servicework] Activate");
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] Removing old cache shell", key);
+
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("[ServiceWorker] Fetch");
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
