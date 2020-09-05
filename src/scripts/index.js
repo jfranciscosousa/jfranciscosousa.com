ace646e3941715611f2517633849cc075170df4f
José Sousa <josesousa9000@gmail.com>
2020-09-05 12:55:22 +0100

    Optimize

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,2 @@
-if (!window.Promise) import("promise-polyfill/src/polyfill");
-
 require("./lib/themeToggle").init();
-require("./lib/fontLoader").init();
 require("./lib/handleSw").init();
--- src/scripts/turbolinks.js
+++
@@ -1,3 +0,0 @@
-// Require this to make instant page transitions
-require("turbolinks").start();
-require("./lib/prefetchTurbolinks").init();
