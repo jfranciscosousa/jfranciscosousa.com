28670b706fd881c4ba5b1b774a064d9c0bc1bae4
José Sousa <josesousa9000@gmail.com>
2020-09-27 12:54:43 +0100

    Lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ci: {
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "unused-javascript": "off",
+        "uses-rel-preconnect": "off",
+        "render-blocking-resources": "off",
+        "uses-long-cache-ttl": "off",
+      },
+    },
+  },
+};
