21cd406225f88f661a43b3d1af67bdc2fb47855c
Francisco Sousa <francisco.sousa@hey.com>
2021-02-27 16:10:05 +0000

    Fix

--- eleventy/transforms.js
+++ eleventy/transforms.js
@@ -1,18 +1,4 @@
-const { PurgeCSS } = require("purgecss");
-const { JSDOM } = require("jsdom");
 const htmlmin = require("html-minifier");
-
-function contentWithoutStyles(content) {
-  const contentDom = new JSDOM(content);
-
-  const styleTags = contentDom.window.document.querySelectorAll("style");
-
-  styleTags.forEach((styleTag) => {
-    styleTag.remove();
-  });
-
-  return contentDom.serialize();
-}
 
 module.exports = {
   htmlmin: (content, outputPath) => {
@@ -28,35 +14,5 @@ module.exports = {
     }
 
     return content;
-  },
-
-  purgeInlinedCss: async (content, outputPath) => {
-    if (!outputPath.endsWith(".html")) return content;
-
-    const cleanContent = contentWithoutStyles(content);
-    const contentDom = new JSDOM(content);
-    const styleTags = contentDom.window.document.querySelectorAll(
-      "style[data-purge]",
-    );
-
-    for (const styleTag of styleTags) {
-      const styles = styleTag.innerHTML;
-
-      const purgeCSSResults = await new PurgeCSS().purge({
-        content: [{ raw: cleanContent }],
-        css: [{ raw: styles }],
-        keyframes: true,
-        whitelist: ["no-js", "has-js", "no-font", "has-font"],
-        whitelistPatterns: [/data-theme$/, /font-face/],
-      });
-
-      if (purgeCSSResults[0].css && purgeCSSResults[0].css !== "") {
-        styleTag.innerHTML = purgeCSSResults[0].css;
-      } else {
-        styleTag.remove();
-      }
-    }
-
-    return contentDom.serialize();
   },
 };
--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
-    isProd &&
-      require("postcss-import")({
-        path: ["./assets/styles"],
-        resolve: (id, basedir, importOptions) => {
-          if (id.startsWith("./")) return id.replace("./", "");
-
-          return id;
-        },
-      }),
-
     require("tailwindcss"),
 
     isProd && require("cssnano")({ preset: "advanced" }),
