806a870b403283d3349c119d95ead36a32479756
Francisco Sousa <josesousa9000@gmail.com>
2022-05-23 17:51:05 +0100

    Update siteData.ts

--- src/lib/siteData.ts
+++ src/lib/siteData.ts
@@ -141,7 +141,7 @@ const data = {
 		},
 		{
 			name: 'Muro Training Center',
-			href: 'https://murotraining.com',
+			href: 'https://web.archive.org/web/20220523164114/https://murotraining.com/',
 			description: 'Static website. Designed by Sketic. Developed by me.',
 			tags: 'gatsby, react, css-modules, netlify'
 		}
