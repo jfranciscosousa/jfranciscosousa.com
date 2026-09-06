5928912c41a23f85f4ecd6be41326660da38a8c7
Francisco Sousa <francisco.sousa@hey.com>
2026-09-06 21:10:12 +0100

    Update Twitter footer link to X

--- src/lib/siteData.js
+++ src/lib/siteData.js
@@ -1,23 +1,18 @@
 const data = {
   siteUrl: "https://jfranciscosousa.com",
   socials: {
-    twitter: {
-      name: "Twitter",
+    x: {
+      name: "X",
       rel: "noopener external",
-      href: "https://twitter.com/goodxicosousa",
+      href: "https://x.com/goodxicosousa",
       value: "@goodxicosousa",
       icon: `<svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-twitter"
-      ><path
-        d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-      /></svg
+      fill="currentColor"
+      aria-hidden="true"
+      class="icon-x"
+      ><path d="M18.901 1.153h3.68l-8.039 9.187L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932 6.064-6.932Zm-1.291 19.49h2.039L6.486 3.24H4.298L17.61 20.644Z" /></svg
     >
     `,
     },
